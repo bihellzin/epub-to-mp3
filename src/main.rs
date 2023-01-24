b4efe663f2e4c4badf0d473b55c01f58d0cb70f1
bielzin <oliva.gabriel@outlook.com>
2023-01-23 22:31:40 -0300

    manages to have tts working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 use std::env;
+use tts::*;
 
 mod epub_local;
 fn main() {
     let path = PathBuf::from(env::args_os().nth(1).expect("no file given"));
 
     epub_local::read_and_write(path);
+
+    let mut tts = Tts::default();
+    tts.expect("").speak("hello world".to_string(), false);
 }
